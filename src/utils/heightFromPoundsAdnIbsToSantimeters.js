dfae0cc20fcb1b39f813cc0ee3a8c4378bc2acd3
Nazar Shcherbyna <shherbina.nazar@gmail.com>
2021-12-25 12:46:11 +0200

    fix: rewrote converting height

--- src/utils/heightFromPoundsAdnIbsToSantimeters.js
+++ src/utils/heightFromPoundsAdnIbsToSantimeters.js
@@ -2,6 +2,9 @@ const heightFromPoundsAdnIbsToSantimeters = height => {
     const heightInPoundsAndIbs = height.split("'");
     const heightInIbs = heightInPoundsAndIbs[1].split('"');
     const heightInSantimeters = heightInPoundsAndIbs[0] * 30.48 + heightInIbs[0] * 2.54;
-    return heightInSantimeters.toFixed();
+    if (heightInSantimeters < 200) {
+        return `1м ${(heightInSantimeters % 100).toFixed()}см`
+    }
+    return `2м ${(heightInSantimeters % 200).toFixed()}см`
 }
 export default heightFromPoundsAdnIbsToSantimeters
